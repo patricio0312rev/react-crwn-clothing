043026b9d5271365302fbde0622c7c351d36a52d
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-29 13:00:16 -0500

    Creando componentes reutilizables de directorio y menuItem

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import './menu.styles.scss';
+import './menu-item.styles.scss';
 
 const MenuItem = ({ title }) => (
     <div className="menu-item">
         <div className="content">
-            <h1 className="title">{ title }</h1>
+            <h1 className="title">{ title.toUpperCase() }</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
