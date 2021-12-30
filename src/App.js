a5473cf55a2391f3e6bb9774ebfdbbb5b826af4b
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-30 10:43:35 -0500

    Integracion con Firebase terminada

--- src/App.js
+++ src/App.js
@@ -7,18 +7,44 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import Auth from './pages/auth/auth.component';
 
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/sign-in" component={Auth} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/sign-in" component={Auth} />
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -39,8 +39,10 @@ class SignIn extends React.Component {
 
                     <FormInput type="password" name="password" id="password" value={this.state.password} handleChange={this.handleSubmit} label="Password" required />
 
-                    <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>{' '}Sign In With Google{' '}</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn >{' '}Sign In With Google{' '}</CustomButton>
+                    </div>
                 </form>
             </div>
         )
