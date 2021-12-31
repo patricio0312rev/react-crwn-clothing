bd003acb433fd6720071801b3683c990105d1510
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-31 16:07:00 -0500

    Registro de usuarios con correo electronico y contraseña en Firebase terminado

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import './auth.styles.scss';
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const Auth = () => (
-    <div className="auth"><SignIn/></div>
+    <div className="auth">
+        <SignIn/>
+        <SignUp/>
+    </div>
 );
 
 export default Auth;
