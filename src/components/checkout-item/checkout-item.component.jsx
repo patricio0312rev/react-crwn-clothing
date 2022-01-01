e43589782c25c1a8624508dcb547bc08d07b89ec
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-01 17:26:44 -0500

    Agregando funcionalidades de añadir y restar unidades de items en componente CheckoutItem usando CartRedux y metodos utils

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './checkout-item.styles.scss';
-import { clearItemFromCart } from './../../redux/cart/cart.actions';
+import { clearItemFromCart, addItem, removeItem } from './../../redux/cart/cart.actions';
 import { connect } from 'react-redux';
 
-const CheckoutItem = ({ cartItem, clearItem}) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
 
     return (
@@ -13,7 +13,11 @@ const CheckoutItem = ({ cartItem, clearItem}) => {
             </div>
     
             <span className="name">{ name }</span>
-            <span className="quantity">{ quantity }</span>
+            <span className="quantity">
+                <div className="arrow" onClick={() => removeItem(cartItem)} >&#10094;</div>
+                <div className="value">{ quantity }</div>
+                <div className="arrow" onClick={() => addItem(cartItem)} >&#10095;</div>
+            </span>
             <span className="price">{ price }</span>
             
             <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
@@ -22,7 +26,9 @@ const CheckoutItem = ({ cartItem, clearItem}) => {
 };
 
 const mapDispatchToProps = dispatch => ({
-    clearItem: item => dispatch(clearItemFromCart(item))
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item)),
 })
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
