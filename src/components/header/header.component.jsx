f1b9f821cf2bf77227135b3218a336e8bb66261c
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-01 11:28:52 -0500

    Creando el componente CartIcon para nuestro header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import './header.styles.scss'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from './../cart-icon/cart-icon.component';
 
 const Header = ({ currentUser }) => {
     return (
@@ -21,6 +22,7 @@ const Header = ({ currentUser }) => {
                     :
                     <Link className='option' to='/sign-in'>SIGN IN</Link>
                 }
+                <CartIcon/>
             </div>
         </div>
     )
