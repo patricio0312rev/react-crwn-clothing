84d0dd27f44cd322d631d67c16f55eb4b34b03a0
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-09 18:27:13 -0500

    Convirtiendo el metodo fetchCollectionAsync de Thunk a Saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,30 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
+import { firestore, convertCollectionsSnapshotToMap } from './../../firebase/firebase.utils';
+import { fetchCollectionsFailure, fetchCollectionsSuccess } from "./shop.actions";
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync(){
     yield console.log('I am fired');
+
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error));
+    }
+
+    /* What we had when using thunk
+        collectionRef.get().then((snapshot) => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionsMap));
+
+            this.setState({ loading: false });
+        }).catch((error) => dispatch(fetchCollectionsFailure(error)));
+    */
 }
 
 export function* fetchCollectionsStart() {
