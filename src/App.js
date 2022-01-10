455ad50d21e7548cac8c74a54ec5d49bfa671859
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-09 20:01:31 -0500

    Refactorizando el codigo en sagas para metodos de SignInSuccess y SignInFailure

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Auth from './pages/auth/auth.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 
@@ -20,9 +19,11 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
 
-    /* this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    /* 
+    const { setCurrentUser } = this.props;
+    
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         
@@ -35,7 +36,8 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
-    }, (error) => console.log(error)); */
+    }, (error) => console.log(error)); 
+    */
   }
 
   componentWillUnmount() {
@@ -63,8 +65,10 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = dispatch => ({
+/*const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+  export default connect(mapStateToProps, mapDispatchToProps)(App);
+});*/
+
+export default connect(mapStateToProps)(App);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,7 +2,7 @@ import { takeLatest, put, all, call } from "redux-saga/effects";
 
 import UserActionTypes from './user.types';
 import { auth, googleProvider, createUserProfileDocument } from "../../firebase/firebase.utils";
-import { googleSignInFailure, googleSignInSuccess, emailSignInSuccess, emailSignInFailure } from './user.actions';
+import { signInSuccess, signInFailure } from './user.actions';
 
 
 export function* signInWithGoogle() {
@@ -11,9 +11,9 @@ export function* signInWithGoogle() {
         const userRef = yield call(createUserProfileDocument, user);
         const userSnapshot = yield userRef.get();
 
-        yield put(googleSignInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
+        yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
     } catch(error) {
-        yield put(googleSignInFailure(error));
+        yield put(signInFailure(error));
     }
 }
 
@@ -23,9 +23,9 @@ export function* signInWithEmail({ payload: {email, password} }) {
         const userRef = yield call(createUserProfileDocument, user);
         const userSnapshot = yield userRef.get();
         
-        yield put(emailSignInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
+        yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
     } catch(error) {
-        yield put(emailSignInFailure(error));
+        yield put(signInFailure(error));
     }
 }
 
