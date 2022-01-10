49e1cc48d31a408a75b4514448a0dfe6ec70bc11
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-09 19:53:27 -0500

    Configurando EmailSignIn en Sagas"

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,8 +6,7 @@ import './sign-in.styles.scss';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from './../custom-button/custom-button.component';
 
-import { auth } from './../../firebase/firebase.utils';
-import { googleSignInStart } from "../../redux/user/user.actions";
+import { emailSignInStart, googleSignInStart } from "../../redux/user/user.actions";
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -22,14 +21,17 @@ class SignIn extends React.Component {
     handleSubmit = async event => {
         event.preventDefault();
 
+        const { emailSignInStart } = this.props;
         const { email, password } = this.state;
 
-        try {
+        emailSignInStart(email, password);
+
+        /* try {
             await auth.signInWithEmailAndPassword(email, password);
             this.setState({ email: '', password: '' });
         } catch(error) {
             console.log(error);
-        }
+        } */
     }
 
     handleChange = event => {
@@ -63,6 +65,7 @@ class SignIn extends React.Component {
 
 const mapDispatchToProps = (dispatch) => ({
     googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({email, password})),
 });
 
 export default connect(null, mapDispatchToProps)(SignIn);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,12 +2,12 @@ import { takeLatest, put, all, call } from "redux-saga/effects";
 
 import UserActionTypes from './user.types';
 import { auth, googleProvider, createUserProfileDocument } from "../../firebase/firebase.utils";
-import { googleSignInFailure, googleSignInSuccess } from './user.actions';
+import { googleSignInFailure, googleSignInSuccess, emailSignInSuccess, emailSignInFailure } from './user.actions';
 
 
 export function* signInWithGoogle() {
     try {
-        const {user} = yield auth.signInWithPopup(googleProvider);
+        const { user } = yield auth.signInWithPopup(googleProvider);
         const userRef = yield call(createUserProfileDocument, user);
         const userSnapshot = yield userRef.get();
 
@@ -17,12 +17,29 @@ export function* signInWithGoogle() {
     }
 }
 
+export function* signInWithEmail({ payload: {email, password} }) {
+    try {
+        const { user } = yield auth.signInWithEmailAndPassword(email, password);
+        const userRef = yield call(createUserProfileDocument, user);
+        const userSnapshot = yield userRef.get();
+        
+        yield put(emailSignInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
+    } catch(error) {
+        yield put(emailSignInFailure(error));
+    }
+}
+
 export function* onGoogleSignInStart() {
-    yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
+    yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
+}
+
+export function* onEmailSignInStart() {
+    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
 export function* userSagas() {
     yield all([
         call(onGoogleSignInStart),
+        call(onEmailSignInStart),
     ]);
 }
