2f9d0911140812e84609ce61f321875e3cdab177
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-09 21:31:06 -0500

    Eliminando imports que no utilizamos en el componente Header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
-import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 
 import CartIcon from './../cart-icon/cart-icon.component';
