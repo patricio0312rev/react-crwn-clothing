b722739707dc5a01ce5dc63313ce8f2e16b4cd0d
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-09 20:04:26 -0500

    Refactorizando el codigo de UserSagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,10 +5,9 @@ import { auth, googleProvider, createUserProfileDocument } from "../../firebase/
 import { signInSuccess, signInFailure } from './user.actions';
 
 
-export function* signInWithGoogle() {
+export function* getSnapshotFromAuth(userAuth) {
     try {
-        const { user } = yield auth.signInWithPopup(googleProvider);
-        const userRef = yield call(createUserProfileDocument, user);
+        const userRef = yield call(createUserProfileDocument, userAuth);
         const userSnapshot = yield userRef.get();
 
         yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
@@ -17,13 +16,21 @@ export function* signInWithGoogle() {
     }
 }
 
+export function* signInWithGoogle() {
+    try {
+        const { user } = yield auth.signInWithPopup(googleProvider);
+       
+        yield getSnapshotFromAuth(user);
+    } catch(error) {
+        yield put(signInFailure(error));
+    }
+}
+
 export function* signInWithEmail({ payload: {email, password} }) {
     try {
         const { user } = yield auth.signInWithEmailAndPassword(email, password);
-        const userRef = yield call(createUserProfileDocument, user);
-        const userSnapshot = yield userRef.get();
-        
-        yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
+       
+        yield getSnapshotFromAuth(user);
     } catch(error) {
         yield put(signInFailure(error));
     }
